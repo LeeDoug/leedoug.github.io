cc11ea6fe28afba426a84da5fdb86cefa8c02e7e
LeeDoug <48081523+LeeDoug@users.noreply.github.com>
2019-04-02 19:19:48 -0700

    Add files via upload

--- js/WavesMultiple.js
+++ js/WavesMultiple.js
@@ -1,27 +1,23 @@
-
 var camera, scene, renderer;
 var cameraControls;
 var clock = new THREE.Clock();
-var camera2;
+
 var squares;
 var projector = new THREE.Projector();
 var theSelectedSquare = null;
 var theObjects = [];
-
-var theObjects2 = [];
 var canvasWidth, canvasHeight;
 
 var m = 60, n = 60;
 var offset = 2.4;
-var waveRate = 9.0;
+var waveRate = 8.0;
 var curWave = null;
-var waveLimit = 10;
+var waveLimit = 30;
 
 var plainColor = null;
 var nbrColors = 201;
 var colors;
 var raycaster = new THREE.Raycaster();
-var raycaster2 = new THREE.Raycaster();
 
 function squareGeom() {
     var geom = new THREE.Geometry();
@@ -63,12 +59,10 @@ function createMatrixOfSquares(m, n, offset) {
             square.j = j;
             root.add(square);
             theObjects.push(square);
-			theObjects2.push(square);
             squares[i][j] = square;
         }
     }
     scene.add(root);
-	root.rotation.x = 0.1;
 }
 
 var maxHeight = 1.5;
@@ -76,7 +70,7 @@ var minHeight = -1.5;
 var heightRange = maxHeight - minHeight;
 
 function heightFunction(delta, dist) {
-        return maxHeight * Math.cos(0.5*delta);
+        return maxHeight * Math.sin(0.5*delta);
 }
 
 function colorFunction(ht, delta, dist) {
@@ -87,65 +81,27 @@ function colorFunction(ht, delta, dist) {
 
 function updateSquares() {
     var changed = true;
-    for (var i = 0, j = theObjects.length; i < theObjects.length; i++,j--) {
+    for (var i = 0; i < theObjects.length; i++) {
         var obj = theObjects[i];
-		var obj2 = theObjects2[i];
         dist = distance(theSelectedSquare, obj);
         delta = curWave - dist;
         if (delta > waveLimit) {
-            obj.position.z = 0;
-			obj2.position.z=0;
-            obj.material.color = plainColor;obj2.material.color = plainColor;
+            obj.position.z = 5;
+            obj.material.color = plainColor;
         } else if (delta > 0) {
             var ht = heightFunction(delta, dist);
-            obj.position.z = ht*i;
-			
-			obj2.position.z = ht;
-			
+            obj.position.z = ht;
             obj.material.color = colorFunction(ht, delta, dist);
             changed = true;
         }
     }
 	
-	/**
     if (!changed) {
         curWave = null;
         theSelectedSquare.material.color = plainColor;
         theSelectedSquare = null;
         initializeColors();
-    }**/
-	
-}
-
-function updateSquares2() {
-    var changed = true;
-    for (var i = 0, j = theObjects.length; i < theObjects.length; i++,j--) {
-        var obj = theObjects[i];
-		var obj2 = theObjects2[i];
-        dist = distance(theSelectedSquare, obj);
-        delta = curWave - dist;
-        if (delta > waveLimit) {
-            obj.position.z = 5;
-			obj2.position.z=5;
-            obj.material.color = plainColor;obj2.material.color = plainColor;
-        } else if (delta > 0) {
-            var ht = heightFunction(delta, dist);
-            //obj.position.y = ht*i;
-			
-			obj2.position.z = ht;
-			
-            obj2.material.color = colorFunction(ht, delta, dist);
-            changed = true;
-        }
     }
-	
-	/**
-    if (!changed) {
-        curWave = null;
-        theSelectedSquare.material.color = plainColor;
-        theSelectedSquare = null;
-        initializeColors();
-    }**/
 	
 }
 
@@ -158,12 +114,8 @@ function distance(sq1, sq2) {
 
 function createScene() {
     initializeColors();
-	initializeColors2();
     var matrixOfSquares = createMatrixOfSquares(m, n, offset);
     scene.add(matrixOfSquares);
-	
-	var matrix = createMatrixOfSquares(m, n, offset);
-    scene.add(matrix);
 }
 
 function initializeColors() {
@@ -176,73 +128,26 @@ function initializeColors() {
     for (var j = 0; j < nbrColors2; j++) {
         var sat = 1 - j/nbrColors2;
         colors[j] = new THREE.Color().setHSL(hues[0], sat, 0.5);
-        colors[nbrColors-j-1] = new THREE.Color().setHSL(hues[3], sat, 0.5);
+        colors[nbrColors-j-1] = new THREE.Color().setHSL(hues[1], sat, 0.5);
     }
-    plainColor = colors[nbrColors2] = new THREE.Color().setHSL(0, 0.5, 0.7);
+    plainColor = colors[nbrColors2] = new THREE.Color().setHSL(0, 0, 0.5);
 }
-
-function initializeColors2() {
-    if (nbrColors % 2 == 0) {
-        nbrColors++;
-    }
-    colors = new Array(nbrColors);    
-    nbrColors2 = (nbrColors - 1) / 2;
-    var hues = [Math.random(), Math.random()];
-    for (var j = nbrColors2; j >0; j--) {
-        var sat = 1 - j/nbrColors2;
-        colors[j] = new THREE.Color().setHSL(hues[0], sat, 0.5);
-        colors[nbrColors-j-1] = new THREE.Color().setHSL(hues[3], sat, 0.5);
-    }
-    plainColor = colors[nbrColors2] = new THREE.Color().setHSL(0, 0.2, 0.5);
-}
-var x = 3;
 
 
 function onDocumentMouseDown(event) {
-   // var mouse = new THREE.Vector2(
-    //    2*(event.clientX/window.innerWidth)-1,
-      //  1-2*(event.clientY/window.innerHeight));
-		var mouse2 = new THREE.Vector2(0.55,0.55);
-		var mouse = new THREE.Vector2(0.32,0.55);
-		//document.write(window.innerWidth);
-		//document.write(window.innerHeight);
-    //raycaster.setFromCamera(mouse, camera);
-
-	
-	for(var i = 0 ; i<2;i++){
-		raycaster.setFromCamera(mouse2,camera);
-		
-		raycaster.setFromCamera(mouse,camera);
-		
-    var intersects = raycaster.intersectObjects(theObjects,);//array
+    var mouse = new THREE.Vector2(
+        2*(event.clientX/canvasWidth)-1,
+        1-2*(event.clientY/canvasHeight));
+    raycaster.setFromCamera(mouse, camera);
+    var intersects = raycaster.intersectObjects(theObjects);
     if ((!theSelectedSquare) && (intersects.length > 0)) {
         // select the closest intersected object
-		for(var i = 0 ; i<3;i++){
-        theSelectedSquare = intersects[i].object;
-        theSelectedSquare.material.color.setHex( Math.random() * 0xffffff );
+        theSelectedSquare = intersects[0].object;
+        theSelectedSquare.material.color = new THREE.Color(0xff0000);
         curWave = 0;
-		}
     }
 	
-	raycaster2.setFromCamera(mouse,camera2);
-		
-	
-	var intersects2 = raycaster2.intersectObjects(theObjects2);
-    if ((!theSelectedSquare) && (intersects.length > 0)) {
-        // select the closest intersected object
-        theSelectedSquare = intersects[i].object;
-        theSelectedSquare.material.color.setHex( Math.random() * 0xffffff );
-        curWave = 0;
-	
-	}
-
-	
-		//document.write(window.innerWidth);
-		//document.write(window.innerHeight);
-    //raycaster.setFromCamera(mouse, camera);
-	
 	//printMousePos(event);
-}
 }
 
 document.addEventListener('mousedown', onDocumentMouseDown, false);
@@ -255,8 +160,6 @@ function printMousePos(event) {
 
 function animate() {
     window.requestAnimationFrame(animate);
-	document.addEventListener('mousedown', onDocumentMouseDown, false);
-	document.addEventListener('mousedown', onDocumentMouseDown, false);
     render();
 }
 
@@ -267,13 +170,11 @@ function render() {
         curWave += (waveRate * delta);
         updateSquares();
 		delta += 0.1;
-		
-		updateSquares2();
+		updateSquares();
     }
 
     cameraControls.update(delta);
     renderer.render(scene, camera);
-	renderer.render(scene,camera2);
 }
 
 
@@ -295,12 +196,6 @@ function init() {
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
-	 
-	 camera2 = new THREE.PerspectiveCamera( 40, canvasRatio, 1, 1000);
-    camera2.position.set(0, -40, 30);
-    camera2.lookAt(new THREE.Vector3(0, 0, 0));
-
-    cameraControls = new THREE.OrbitControls(camera2, renderer.domElement);
 }
 
 
@@ -320,11 +215,9 @@ function addToDOM() {
     addToDOM();
     render();
     animate();
-	again();
 /**
 } catch(e) {
     var errorMsg = "Error: " + e;
     document.getElementById("msg").innerHTML = errorMsg;
 }
 **/
-
